79164e186444af7531b7c658f088cc697d9418d1
sth <szetszho@gmail.com>
2017-07-20 15:27:25 +0800

    finish scan and show

--- app/src/main/java/action/sth/locationshow/MainActivity.java
+++ app/src/main/java/action/sth/locationshow/MainActivity.java
@@ -1,6 +1,7 @@
 package action.sth.locationshow;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,6 +10,8 @@ import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,6 +25,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String CoarseLocation = Manifest.permission.ACCESS_COARSE_LOCATION;
+    final String AccessWifi = Manifest.permission.ACCESS_WIFI_STATE;
+    final String ChangeWifi = Manifest.permission.CHANGE_WIFI_STATE;
 
     TextView textLocation;
     ListView listWifi;
@@ -40,6 +47,18 @@ public class MainActivity extends AppCompatActivity {
         textLocation = (TextView) findViewById(R.id.textLocation);
         listWifi = (ListView) findViewById(R.id.listWifi);
 
+        if (checkSelfPermission(CoarseLocation) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        }
+
+        if (checkSelfPermission(AccessWifi) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_WIFI_STATE, Manifest.permission.ACCESS_WIFI_STATE}, 123);
+        }
+
+        if (checkSelfPermission(ChangeWifi) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CHANGE_WIFI_STATE, Manifest.permission.CHANGE_WIFI_STATE}, 123);
+        }
+
         wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
         this.adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arraylist);
@@ -47,6 +66,10 @@ public class MainActivity extends AppCompatActivity {
 
         checkWifiStatus();
         //refreshLocation();
+    }
+
+    public void onClickRefreshButton(View view){
+        refreshLocation();
     }
 
     public void checkWifiStatus(){
@@ -58,17 +81,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public String checkScanList(){
-        return "null";
+        if(arraylist.contains("a2:20:a6:21:b9:b5")){
+            return "Test Station One";
+        }
+        else
+            return "nowhere";
     }
-
-    public void refreshLocation(View view){
+    
+    public void refreshLocation(){
         arraylist.clear();
+        adapter.clear();
         checkWifiStatus();
         registerReceiver(wifi_receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
         wifi.startScan();
         Log.d("WifScanner", "scanWifiNetworks");
-        String location = checkScanList();
-        textLocation.setText(location);
     }
 
     BroadcastReceiver wifi_receiver = new BroadcastReceiver() {
@@ -88,8 +114,9 @@ public class MainActivity extends AppCompatActivity {
             }
             catch (Exception e){
                 Log.w("WifiScanner","Exception:"+ e);
+                String location = checkScanList();
+                textLocation.setText(location);
             }
         }
     };
-
 }
